9cfcb71b582482b5962971f0453fc118e37f2175
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-11-02 01:21:12 +0800

    InputReader: add 5-point calibration
    Updated for Android 5.0.

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -1387,6 +1387,9 @@ protected:
                 *outSize = 0;
             }
         }
+
+        // 5-point calibration parameters
+        int fiveCal[7];
     } mCalibration;
 
     // Affine location transformation/calibration
